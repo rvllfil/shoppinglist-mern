f143415134d37c5dcd963b361c90019c6e7fdccb
rvllfil <rullfhiel@gmail.com>
2021-03-07 23:44:16 +0700

    reconfigure store and package.json

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './rootReducer'
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 export default store
